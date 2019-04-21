f97fa2deff9e3cd5f16e12894d1c138f5727753b
Александр <DEDALX305@mail.ru>
2019-04-21 14:01:24 +0500

    0.9.1

--- IPM/WindowsFormsApp2/Form4.Designer.cs
+++ IPM/WindowsFormsApp2/Form4.Designer.cs
@@ -38,6 +38,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -95,7 +97,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(139, 828);
+            this.label2.Location = new System.Drawing.Point(148, 828);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 13);
             this.label2.TabIndex = 5;
@@ -106,7 +108,7 @@
             this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox3.Location = new System.Drawing.Point(398, 41);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(778, 778);
+            this.pictureBox3.Size = new System.Drawing.Size(600, 600);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 6;
             this.pictureBox3.TabStop = false;
@@ -114,7 +116,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(736, 822);
+            this.label3.Location = new System.Drawing.Point(648, 644);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 13);
             this.label3.TabIndex = 7;
@@ -140,11 +142,30 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(398, 670);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(600, 39);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(665, 712);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Цепной код";
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1188, 844);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
@@ -178,5 +199,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
--- IPM/WindowsFormsApp2/Form4.cs
+++ IPM/WindowsFormsApp2/Form4.cs
@@ -221,37 +221,16 @@ namespace IMPSpace
             {
 
             }
-        } // Сглаживание
+        } // Скелитизация
 
-        // Цепной код
-        class chain
+        class chain // Цепной код
         { // Скелетизация! даже в заднице есть выход ¯ \ _ (ツ) _ / ¯ 
-            public Bitmap transformation(Bitmap TempBitmap)
+            public List<int> transformation(Bitmap TempBitmap)
             {
                 Bitmap processed_bitmap = new Bitmap(TempBitmap.Width, TempBitmap.Height);
                 int bytes = TempBitmap.Width * TempBitmap.Height;
                 byte[] color_image = new byte[bytes];
                 byte[,] color_image_matr = new byte[TempBitmap.Width, TempBitmap.Width]; // содание матрицы изображения
-
-                // Создание массива 1 и 0 по бинаризованному изображению
-                /*
-                for (int i = 0; i < TempBitmap.Width; i++)
-                {
-                    for (int j = 0; j < TempBitmap.Height; j++)
-                    {
-                        if (((TempBitmap.GetPixel(i, j).R + TempBitmap.GetPixel(i, j).G + TempBitmap.GetPixel(i, j).B) / 3) == 0)
-                        {
-                            color_image[i + j * TempBitmap.Width] = 1;
-                            //c1++;
-                        }
-                        else if (((TempBitmap.GetPixel(i, j).R + TempBitmap.GetPixel(i, j).G + TempBitmap.GetPixel(i, j).B) / 3) == 255)
-                        {
-                            color_image[i + j * TempBitmap.Width] = 0;
-                            //c2++;
-                        }
-                    }
-                }
-                */
 
                 // Создание матрицы 1 и 0 по бинаризованному изображению
 
@@ -1629,7 +1608,7 @@ namespace IMPSpace
                         if (((b1i + 1) == bi) && ((b1j + 1) == bj))
                         {
                             movement.Add(3);
-                            // Твоему направлению пришщёл конец!
+                            // Твоему направлению пришёл конец!
                         }
 
                         bi = b1i;
@@ -1643,174 +1622,7 @@ namespace IMPSpace
                 } while ((bi != b0iRemember) || (bj != b0jRemember));
 
                 int l = 10;
-                /*
-                  2. 
-                    Пусть b = b1 и c = c.
-                  3.
-                    Начиная с точки c и двигаясь по часовой стрелке, обозначим восьмерку соседей точки b через n1, n2,..., n8. Находим первую точку nk, имеющую значение 1.
-                  4.
-                    Кладем 4. b = nk  и c = nk–1.
-                */
-                for (int i = Width + 1; i < color_image.Length - Width - 1; i++) // Бегаем по массиву 1 и 0 изображения
-                {
-                    /* Условия для первого шага
-                      (а) 2 ≤ N(p1) ≤ 6
-                      (б) T(p1) = 1
-                      (в) p2 * p4 * p6 = 0
-                      (г) p4 * p6 * p8 = 0
-                      (д) Если у тебя есть рот, то ты сосёшь и ты пидор
-                    */
-
-                    // Кстати, тебе тут привет от трёх лиц.... от хуя и двух яиц
-                    int condition_a = 0, condition_b = 0, condition_v = 0, condition_g = 0; // Обьявление пустых переменных для условий
-                    int T_p = 0; // Обьявление пустой переменной для условия (б)
-                    int N_p1 = ( // Обьявление переменной для условия (а)
-                        color_image[i - Width] +     // P2
-                        color_image[i - Width + 1] + // P3
-                        color_image[i + 1] +         // P4
-                        color_image[i + Width + 1] + // P5
-                        color_image[i + Width] +     // P6
-                        color_image[i + Width - 1] + // P7
-                        color_image[i - 1] +         // P8
-                        color_image[i - Width - 1]); // P9  
-
-                    // Задание массива для проверки условий (в) и (г)
-                    byte[] array_1_and_0 = new byte[8];
-                    array_1_and_0[0] = color_image[i - Width];      // P2 
-                    array_1_and_0[1] = color_image[i - Width + 1];  // P3
-                    array_1_and_0[2] = color_image[i + 1];          // P4
-                    array_1_and_0[3] = color_image[i + Width + 1];  // P5
-                    array_1_and_0[4] = color_image[i + Width];      // P6
-                    array_1_and_0[5] = color_image[i + Width - 1];  // P7
-                    array_1_and_0[6] = color_image[i - 1];          // P8
-                    array_1_and_0[7] = color_image[i - Width - 1];  // P9
-
-                    // Сергей жружко маг? конечно нет
-                    // Проверка условия (а) N(p1) - Число ненулевых соседей элемента p1 N(p1)=p2 + p3 + .... p8 + p9
-                    if ((N_p1 >= 2) && (N_p1 <= 6)) condition_a = 1;
-
-                    // Проверка условия (б) T(p1) = 1 T(p1) — число переходов 0—1 в упорядоченной последовательности p2, p3, ... , p8, p9, p2
-                    for (int j = 0; j < 7; j++) // Проверка перехода P2...P8
-                    {
-                        if ((array_1_and_0[j] == 0) && (array_1_and_0[j + 1] == 1))
-                        {
-                            T_p++;
-                        }
-                    }
-
-                    // Проверка условия (д). У меня для тебя плохие новости...
-                    if ((array_1_and_0[7] == 0) && (array_1_and_0[0] == 1))
-                    {
-                        T_p++; // Проверка перехода P9 и P2
-                    }
-                    if (T_p == 1)
-                    {
-                        condition_b = 1;
-                    }
-
-                    // Как долго я ждал этого момента мой маленький зеленый дружок
-                    // Проверка условия (в) p2 * p4 * p6 = 0
-                    if ((color_image[i - Width] * // P2
-                        color_image[i + 1] * // P4
-                        color_image[i + Width]) == 0) condition_v = 1; // P6
-
-                    // Проверка условия (г) p4 · p6 · p8 = 0
-                    if ((color_image[i + 1] * // P4
-                        color_image[i + Width] * // P6
-                        color_image[i - 1]) == 0) condition_g = 1; // P8
-
-                    // Создаем новое изображения после первого прохода
-                    if ((condition_a + condition_b + condition_v + condition_g) == 4)
-                    {
-                        color_image[i] = 0;
-                    }
-                }
-
-                for (int i = Width + 1; i < color_image.Length - Width - 1; i++) // Бегаем по массиву 1 и 0 изображения
-                {
-
-                    /* Условия для первого шага
-                      (а) 2 ≤ N(p1) ≤ 6
-                      (б) T(p1) = 1
-                      (в) p2 * p4 * p8 = 0
-                      (г) p2 * p6 * p8 = 0
-                    */
-                    int condition_a = 0, condition_b = 0, condition_v = 0, condition_g = 0; // Обьявление пустых переменных для условий
-                    int T_p = 0; // Обьявление пустой переменной для условия (б)
-                    int N_p1 = ( // Обьявление переменной для условия (а)
-                        color_image[i - Width] +     // P2
-                        color_image[i - Width + 1] + // P3
-                        color_image[i + 1] +         // P4
-                        color_image[i + Width + 1] + // P5
-                        color_image[i + Width] +     // P6
-                        color_image[i + Width - 1] + // P7
-                        color_image[i - 1] +         // P8
-                        color_image[i - Width - 1]); // P9  
-
-                    // Задание массива для проверки условий (в) и (г)
-                    byte[] array_1_and_0 = new byte[8];
-                    array_1_and_0[0] = color_image[i - Width];      // P2 
-                    array_1_and_0[1] = color_image[i - Width + 1];  // P3
-                    array_1_and_0[2] = color_image[i + 1];          // P4
-                    array_1_and_0[3] = color_image[i + Width + 1];  // P5
-                    array_1_and_0[4] = color_image[i + Width];      // P6
-                    array_1_and_0[5] = color_image[i + Width - 1];  // P7
-                    array_1_and_0[6] = color_image[i - 1];          // P8
-                    array_1_and_0[7] = color_image[i - Width - 1];  // P9
-
-                    // Сергей жружко маг? конечно нет
-                    // Проверка условия (а) N(p1) - Число ненулевых соседей элемента p1 N(p1)=p2 + p3 + .... p8 + p9
-                    if ((N_p1 >= 2) && (N_p1 <= 6)) condition_a = 1;
-
-                    // Проверка условия (б) T(p1) = 1 T(p1) — число переходов 0—1 в упорядоченной последовательности p2, p3, ... , p8, p9, p2
-                    for (int j = 0; j < 7; j++) // Проверка перехода P2...P8
-                    {
-                        if ((array_1_and_0[j] == 0) && (array_1_and_0[j + 1] == 1))
-                        {
-                            T_p++;
-                        }
-                    }
-                    if ((array_1_and_0[7] == 0) && (array_1_and_0[0] == 1))
-                    {
-                        T_p++; // Проверка перехода P9 и P2
-                    }
-                    if (T_p == 1)
-                    {
-                        condition_b = 1;
-                    }
-
-                    // Как долго я ждал этого момента мой маленький зеленый дружок
-                    // Проверка условия (д) p2 * p4 * p8 = 0
-                    if ((color_image[i - Width] * // P2
-                        color_image[i + 1] * // P4
-                        color_image[i - 1]) == 0) condition_v = 1; // P8
-
-                    // Проверка условия (Е) p2 · p6 · p8 = 0
-                    if ((color_image[i - Width] * // P2
-                        color_image[i + Width] * // P6
-                        color_image[i - 1]) == 0) condition_g = 1; // P8
-
-                    // Создаем новое изображения после первого прохода
-                    if ((condition_a + condition_b + condition_v + condition_g) == 4)
-                    {
-                        color_image[i] = 0;
-                    }
-                }
-
-                // Создаем изображдение из обработанного массива
-                for (int i = 0; i < color_image.Length; i++)
-                {
-                    if (color_image[i] == 1)
-                    {
-                        processed_bitmap.SetPixel((i % Width), (i / Width), Color.Black);
-                    }
-                    if (color_image[i] == 0)
-                    {
-                        processed_bitmap.SetPixel((i % Width), (i / Width), Color.White);
-                    }
-                }
-
-                return processed_bitmap;
+                return movement;
             }
 
 
@@ -1895,11 +1707,19 @@ namespace IMPSpace
 
         private void button4_Click(object sender, EventArgs e)
         {
-            chain test = new chain();
+            pictureBox3.Image = null;
+            richTextBox1.Text = null;
+
+            chain obj_chain = new chain();
             binarization binar = new binarization();
 
             bitNew = binar.BitmapToBlackWhite2(BitmapBinarization);
-            pictureBox3.Image = (Image)test.transformation(bitNew);
+            List<int> List_chain = obj_chain.transformation(bitNew);
+
+            for (int i = 0; i < List_chain.Count; i++)
+            {
+                richTextBox1.Text += List_chain[i].ToString(); // + Environment.NewLine;  + "\r\n"
+            }
         }
     }
 }
